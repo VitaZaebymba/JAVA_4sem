e2fb263dc29353ca5aa349cfe948efc7391671aa
Vita <malavita29@mail.ru>
2021-04-17 11:29:47 +0300

    first commit

--- src/ru/mirea/ikbo1319/pr2/Main.java
+++ src/ru/mirea/ikbo1319/pr2/Main.java
@@ -2,7 +2,7 @@ package ru.mirea.ikbo1319.pr2;
 
 public class Main {
 
-    public static void main(String[] args) {
+    public static xdggsgvoid main(String[] args) {
 	// write your code here
     }
 }
